9b79ead0ef3dadca29ba22a80f190652f2b57a25
German Popoter <g.popoteur@gmail.com>
2013-11-04 13:07:31 +0100

    agregando bootstrap 3 a la aplicacion

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib');
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+/**
+ * Configuracion de View Engine
+ * y Estilo
+ */
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { 
+  	src: __dirname + '/public', 
+  	compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
 
-app.listen(3000);
+// Inicio de la App
+app.get('/', function (req, res) {
+   res.render('home/index',
+  		{ 
+  			title : 'Home' 
+  		}
+  	)
+})
+
+app.listen(3000)
