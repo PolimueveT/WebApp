897f48e34e53919bebc32a36905d048c64fa9190
German Popoter <g.popoteur@gmail.com>
2013-11-11 09:45:21 +0100

    Haciendo pulo

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.use(stylus.middleware(
 ))
 app.use(express.static(__dirname + '/public'))
 
+// MOCK
+var Parking = function(nombre, plazas, cant_libre){
+	var self = this;
+	self.nombre = nombre;
+	self.plazas = plazas;
+	self.cant_libre = cant_libre;
+
+	return self;
+};
+
 // Inicio de la App
 app.get('/', function (req, res) {
    res.render('home/index',
@@ -30,6 +40,33 @@ app.get('/', function (req, res) {
   			title : 'Home' 
   		}
   	)
-})
+});
+
+// Estado Parking
+app.get('/estado-parking', function (req, res) {
+	var parkingsArray = [];
+
+	for (var i = 0; i <= 5; i++) {
+		var plazas = i * 10;
+		var p = new Parking('nombre' + i, plazas, plazas/2 );
+		parkingsArray.push(p);
+	}
+
+   res.render('home/estado_parking',
+  		{ 
+  			title : 'Estado Parking',
+  			parkings: parkingsArray
+  		}
+  	)
+});
+
+// Inicio de la App
+app.get('/trayectos', function (req, res) {
+   res.render('home/index',
+  		{ 
+  			title : 'Home' 
+  		}
+  	)
+});
 
 app.listen(3000)
