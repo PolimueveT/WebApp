26c969ff91855c1fe104fa2508b240e6cf327c94
Cesar Luis <cesar.luis91@gmail.com>
2013-11-13 00:01:18 +0100

    API trayectos GET y POST funcionando

--- api/controller/TripController.js
+++ api/controller/TripController.js
@@ -1,86 +1,118 @@
     var TripData=require('../data/TripData');
     
 
-var crearData=function(body){
+    var crearData=function(body){
 
-    var num_plazas = body.num_plazas;
-       var origen = body.origen;
-       var destino = body.destino;
-       var hora_salida = body.hora_salida;
-       var precio_plaza = body.precio_plaza;
-       var tiempo_max_espera = body.tiempo_max_espera;
-       var restricciones = body.restricciones;
-       var max_tamaño_equipaje = body.max_tamaño_equipaje;
-       var tipo_pasajero = body.tipo_pasajero;
-       var observaciones = body.observaciones;
-       var creador_id = body.creador_id;
-       var inscritos = body.inscritos;
+      var num_plazas = body.num_plazas;
+      var origen = body.origen;
+      var destino = body.destino;
+      var hora_salida = body.hora_salida;
+      var precio_plaza = body.precio_plaza;
+      var tiempo_max_espera = body.tiempo_max_espera;
+      var restricciones = body.restricciones;
+      var max_tamaño_equipaje = body.max_tamaño_equipaje;
+      var tipo_pasajero = body.tipo_pasajero;
+      var observaciones = body.observaciones;
+      var creador_id = body.creador_id;
+      var inscritos = body.inscritos;
+
+      delete TripData['_id'];
+      //TripData._id=null;
+      TripData.Num_plazas=null;
+      TripData.Origen=null;
+      TripData.Destino=null;
+      TripData.Hora_salida=null;
+      TripData.Precio_plaza=null;
+      TripData.Tiempo_max_espera=null;
+      TripData.Restricciones=null;
+      TripData.Max_tamaño_equipaje=null;
+      TripData.Tipo_pasajeros=null;
+      TripData.Observaciones=null;
+      TripData.Creador_id=null;
+      TripData.Inscritos=null;
 
 
 
 
+      TripData.Num_plazas=num_plazas;
+      TripData.Origen=origen;
+      TripData.Destino=destino;
+      TripData.Hora_salida=hora_salida;
+      TripData.Precio_plaza=precio_plaza;
+      TripData.Tiempo_max_espera=tiempo_max_espera;
+      TripData.Restricciones=restricciones;
+      TripData.Max_tamaño_equipaje=max_tamaño_equipaje;
+      TripData.Tipo_pasajeros=tipo_pasajero;
+      TripData.Observaciones=observaciones;
+      TripData.Creador_id=creador_id;
+      TripData.Inscritos=inscritos;
 
+      console.log('INSERT =' + JSON.stringify(TripData));
+      return TripData;
 
-       TripData.Num_plazas=num_plazas;
-       TripData.Origen=origen;
-       TripData.Destino=destino;
-       TripData.Hora_salida=hora_salida;
-       TripData.Precio_plaza=precio_plaza;
-       TripData.Tiempo_max_espera=tiempo_max_espera;
-       TripData.Restricciones=restricciones;
-       TripData.Max_tamaño_equipaje=max_tamaño_equipaje;
-       TripData.Tipo_pasajeros=tipo_pasajero;
-       TripData.Observaciones=observaciones;
-       TripData.Creador_id=creador_id;
-       TripData.Inscritos=inscritos;
-       return TripData;
-
-};
+    };
 
 
     var TripController = function(TripDAO) {
-        var _TripDAO = TripDAO;
+      var _TripDAO = TripDAO;
 
-        var objetoRespuesta = {
-            success: false,
-            info: null,
-            data: null
-        };
+      var objetoRespuesta = {
+        success: false,
+        info: null,
+        data: null
+      };
 
-        //console.log(objetoRespuesta);
+        
 
       //Por hacer
-      this.getTrip = function(req, res, callback) {
+      this.getTrip = function(req, res) {
         var name = req.params.name;
         console.log('name: ' + name);
         // validar / parsear name...
-        _TripDAO.readUser(name, function(err, user) {
-            if(err) {
-              console.log("err: "+err);
-              return callback(err);  
+        _TripDAO.readTrip(name, function(err, user) {
+          if(err) {
+            console.log("err: "+err);
+            return callback(err);  
           } 
-      });
-    };
-
-    this.addTrip = function(req, res) {
-
-      _tripdata= crearData(req.body);
-       
+        });
+      };
 
 
-      // console.log(num_plazas +" "+ origen +" "+ destino);
+ 
+      this.getTrips = function(req, res) {
+             
+        _TripDAO.readTrips( function(err,trips) {
+          if(err) {
+             console.log('Error TripController');
+            objetoRespuesta.success=false;                
+            objetoRespuesta.info=err;
+            objetoRespuesta.data=null;
+            res.send(objetoRespuesta);
+            return;
+          } 
+
+          objetoRespuesta.success=true;                
+            objetoRespuesta.info="Se han leido correctamente todos los trayectos";
+            objetoRespuesta.data=trips;
+            res.send(objetoRespuesta);
 
 
-        // validar / parsear name...
+        });
+      }; 
+
+      this.addTrip = function(req, res) {
+        console.log('request =' + JSON.stringify(req.body))
+        _tripdata= crearData(req.body);
+
+          // validar / parsear name...
         _TripDAO.insertTrip(_tripdata, function(err) {
-            if(err){
-              console.log('Error TripController');
-              objetoRespuesta.success=false;                
-              objetoRespuesta.info=err;
-              objetoRespuesta.data=null;
-
-              res.send(objetoRespuesta);
-              return;
+          if(err){
+            console.log('Error TripController');
+            objetoRespuesta.success=false;                
+            objetoRespuesta.info=err;
+            objetoRespuesta.data=null;
+            res.send(objetoRespuesta);
+            return;
           }
 
           objetoRespuesta.success=true;                
@@ -90,9 +122,9 @@ var crearData=function(body){
 
 
 
-      });
+        });
+      };
+
     };
 
-};
-
-module.exports = TripController;
+    module.exports = TripController;
--- api/dao/TripDAO.js
+++ api/dao/TripDAO.js
@@ -1,6 +1,6 @@
 var TripDAO = function(db) {
-    
-    var _db = db;
+
+  var _db = db;
 
     /**
      *
@@ -24,66 +24,94 @@ var TripDAO = function(db) {
 
      //Por hacer
      this.readTrip = function(name, callback) {
-        if(!name) {
-            console.log('dao receive no name');
-            return callback(new Error("Nombre invalido."));
-        }
-        else{
+      if(!name) {
+        console.log('dao receive no name');
+        return callback(new Error("Nombre invalido."));
+      }
+      else{
 
-        }
-        
+      }
+
     };
+
+
+
+
+         //Por hacer
+         this.readTrips = function(callback) {
+           console.log('Ejecutando el get');
+           _db.collection("trayectos",function(err,collection){
+
+            collection.find().toArray(function (err,trips){
+
+                        if(err){
+            console.log('Error consultando en collection trayectos');
+            return callback(err,null);
+
+          }
+
+          console.log('Éxito consultando en collection trayectos');
+          return callback(null,trips);
+
+
+           
+            });
+
+           });
+
+         };
 
       //funciona pero falta control de errores/respuesta y revisar si la estructura es adecuada
-    this.insertTrip = function(tripdata, callback) {
-      console.log('Ejecutando el post');
-     _db.collection("trayectos",function(err,collection){
+      this.insertTrip = function(tripdata, callback) {
+        var _tripdata=tripdata;
+        console.log('Ejecutando el post');
+        _db.collection("trayectos",function(err,collection){
 
-       collection.insert( tripdata ,function (err,result){
-        
-        if(err){
-          console.log('Error insertando en collection trayectos');
-          return callback(err);
+         collection.insert(_tripdata ,function (err,result){
 
-        }
-        
+          if(err){
+            console.log('Error insertando en collection trayectos');
+            return callback(err);
+
+          }
+
           console.log('Éxito insertando en collection trayectos');
-           return callback(null);
-        
+          return callback(null);
 
 
-      });
 
-      });
-    
-  };
+        });
+
+       });
+
+      };
 
       //Por hacer
-     this.updateTrip = function(name, callback) {
+      this.updateTrip = function(name, callback) {
         if(!name) {
-            console.log('dao receive no name');
-            return callback(new Error("Nombre invalido."));
+          console.log('dao receive no name');
+          return callback(new Error("Nombre invalido."));
         }
         else{
 
         }
         
-    };
+      };
 
       //Por hacer
-    this.deleteTrip = function(name, callback) {
+      this.deleteTrip = function(name, callback) {
         if(!name) {
-            console.log('dao receive no name');
-            return callback(new Error("Nombre invalido."));
+          console.log('dao receive no name');
+          return callback(new Error("Nombre invalido."));
         }
         else{
 
         }
         
+      };
+
+
+
     };
 
-
-
-};
-
-module.exports = TripDAO;
+    module.exports = TripDAO;
