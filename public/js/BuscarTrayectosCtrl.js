42294771b12fee4a376aaca5cc1a26c3372884cb
serpeilop <serpeilop@gmail.com>
2013-11-15 18:21:58 +0100

    Buscar trayectos en progreso, necesito Pull

--- public/js/BuscarTrayectosCtrl.js
+++ public/js/BuscarTrayectosCtrl.js
@@ -1,9 +1,9 @@
 var BuscarTrayectosCtrl = function($scope, $http){
 
-	$scope.trayectos [];
+	$scope.trayectos = [];
 
 	$scope.getData = function(){
-		$http.get('/api/gettrips').success(function (result){
+		$http.get('/api/gettrips/').success(function (result){
 			if(result !== undefined){
 				$scope.trayectos = result.data;
 			}
