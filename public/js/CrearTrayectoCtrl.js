fe96450660f2a2ac18181d225bcbc37231ecbcb5
German Popoter <g.popoteur@gmail.com>
2013-11-18 11:37:43 +0100

    Validacion del lado del cliente para crear trayectos

--- public/js/CrearTrayectoCtrl.js
+++ public/js/CrearTrayectoCtrl.js
@@ -1,31 +1,35 @@
 function CrearTrayectoCtrl($scope, $http) {
 
+	$scope.trayecto = {
+		num_plazas: 1,
+		precio: 0,
+		max_espera: 0,
+		tipo_pasajero: { 
+			alumnos: true,
+			profesores: true,
+			personal: true
+		}
+	};
+	$scope.submitted = false;
+
 	$scope.PublicarTrayecto = function () {
-		var obj = { 
-			num_plazas: $scope.num_plazas,
-			origen: $scope.origen,
-			destino: $scope.destino,
-			hora_salida: $scope.hora,
-			precio_plaza: $scope.precio,
-			tiempo_max_espera: $scope.max_espera,
-			restricciones :[],
-			max_tamaño_equipaje: $scope.equipaje,
-			tipo_pasajeros: 1,
-			observaciones: $scope.observaciones,
-			creador_id: 'from_site',
-			inscritos :['bb', 'dd'],
-		};
+		if ($scope.form.$valid) {
+			var obj = angular.copy($scope.trayecto);
 
-		console.log(obj);
+			console.log(obj);
 
-		// Enviamos obj con un POST al server
-		// Por AJAX.
-		$http.post('/api/newtrip', obj).success(function (response){
-			console.log(response);
-			if(response.success === true) { 
-				window.location = "/mis-trayectos";	
-			 }
-		});
+			// Enviamos obj con un POST al server
+			// Por AJAX.
+			// $http.post('/api/newtrip', obj).success(function (response){
+			// 	console.log(response);
+			// 	if(response.success === true) { 
+			// 		window.location = "/mis-trayectos";	
+			// 	 }
+			// });
+
+	    } else {
+	      $scope.form.submitted = true;
+	    }
 	};
 
 }
