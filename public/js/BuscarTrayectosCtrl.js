b4d7acfc9cb30c0de5bb06a573f4d713d5a400a1
serpeilop <serpeilop@gmail.com>
2013-11-18 17:58:27 +0100

    Añadidos datetimepickers Añadidos filtros

--- public/js/BuscarTrayectosCtrl.js
+++ public/js/BuscarTrayectosCtrl.js
@@ -10,4 +10,23 @@ var BuscarTrayectosCtrl = function($scope, $http){
 		});	
 	}
 
+	$scope.FiltrarTrayectos = function () {
+		var obj = { 
+			origen: $scope.salida,
+			destino: $scope.destino,
+			fumador: $scope.noFumadores,
+			animales: $scope.noAnimales,
+			comida: $scope.noComida,
+			max_equipaje: $scope.equipaje,
+		};
+
+		var filtrados = _.filter($scope.trayectos, function(tra){ 
+			return (tra.Origen == $scope.salida)&&(tra.Destino == $scope.destino)&&(tra.Restricciones[0] === $scope.noFumadores)&&(tra.Restricciones[1] === $scope.noAnimales)&&(tra.Restricciones[2] === $scope.noComida)&&(tra.Max_tamaño_equipaje == $scope.noComida) ;
+			
+		});
+
+		$scope.trayectos = filtrados;
+
+	};
+
 }
