251cbb4b7b02936a35c557b5743b0daa0d3bf54b
German Popoter <g.popoteur@gmail.com>
2013-11-25 11:25:18 +0100

    Terminando unirse a trayecto y salir de trayecto.

--- public/js/VerTrayectoCtrl.js
+++ public/js/VerTrayectoCtrl.js
@@ -2,6 +2,14 @@ function VerTrayectoCtrl($scope, $http) {
 
 	$scope.trayecto = {};
 	$scope.current_user = "";
+	$scope.unido_trayecto = false;
+	$scope.avisos = [];
+
+	$scope.getUnidoATrayecto = function (){
+		$http.get('/api/isuserintrip/' + $scope.trayecto._id + '/' + $scope.current_user).success(function(result){
+			console.log(result);
+		});
+	};
 
 	$scope.getTrayecto = function(id){
 		$http.get('/api/gettrip/' + id).success(function(result){
@@ -14,6 +22,8 @@ function VerTrayectoCtrl($scope, $http) {
 				obj.hora_salida = fechaObj.format("hh:mm a");
 
 				$scope.trayecto = obj;
+
+				$scope.getUnidoATrayecto();
 			};
 		});
 	};
@@ -25,13 +35,34 @@ function VerTrayectoCtrl($scope, $http) {
 		};
 
 		$http.put('/api/applytrip', data).success(function(result){
-			console.log(result);
+			var obj = {
+				clase: [],
+				texto: ''
+			};
+
+			if(result.success == false){
+				obj.clase = ['alert-danger'];
+			}else{
+				obj.clase = ['alert-success'];
+				$scope.unido_trayecto = true;
+			}
+
+			obj.texto = result.info;
+
+			$scope.avisos.push(obj);
 		});
 	};
 
 	$scope.salirTrayecto = function () {
-		$http.put('/api/cancelPassenger', data).success(function(result){
+		var data = {
+			tripId: $scope.trayecto._id,
+			personId: $scope.current_user
+		};
 
+		$http.put('/api/cancelPassenger', data).success(function(result){
+			$scope.avisos.push(data.info);
+
+			$scope.unido_trayecto = false;
 		});
 	};
 
