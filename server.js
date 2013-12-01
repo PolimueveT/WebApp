84ea53bcf2c03a43248f5e9ca073e9aca30776b7
Nico <nicodeboni@gmail.com>
2013-12-01 13:59:09 +0100

    pequeños arreglos en server.js

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ io = require('socket.io').listen(server);
  * y Estilo
  */
 
- console.log('environment: ' + app.get('env'));
+console.log('ENV: ' + app.get('env'));
 
- function compile(str, path) {
-  return stylus(str)
-  .set('filename', path)
-  .use(nib())
+function compile(str, path) {
+    return stylus(str)
+    .set('filename', path)
+    .use(nib())
 }
 
 // Configuracion Express
@@ -25,11 +25,40 @@ app.set('view engine', 'jade')
 app.use(express.logger('dev'))
 app.use(stylus.middleware(
 { 
- src: __dirname + '/public', 
- compile: compile
+    src: __dirname + '/public', 
+    compile: compile
 }
 ))
 app.use(express.static(__dirname + '/public'))
+app.use(express.bodyParser());
+
+// NO CACHE
+app.use(function(req, res, next) {
+    res.header('Cache-Control','private'); 
+    next();
+});
+
+var envDbConfig = null;
+
+// development config
+if ('development' === app.get('env')) {
+    envDbConfig = {
+        port: 27017,
+        host: '127.0.0.1',
+        db: 'polimuevet'
+    };
+}
+
+// production config
+if ('production' === app.get('env')) {
+    envDbConfig = {
+        port: 53428,
+        host: "ds053428.mongolab.com",
+        db: 'polimuevet'
+
+    };
+    // TODO: zona horaria es una hora menos que en ESPAÑA!!!
+}
 
 
 // Conexion a MongoDB
@@ -40,10 +69,13 @@ var MongoServer = mongo.Server,
 Db = mongo.Db,
 BSON = mongo.BSONPure;
 
-/*var mongoServer = new MongoServer('127.0.0.1', 27017, {auto_reconnect: true});
-db = new Db('polimuevet', mongoServer, { safe: true });
+var mongoServer = new MongoServer(envDbConfig.host, envDbConfig.port, {auto_reconnect: true});
+db = new Db(envDbConfig.db, mongoServer, { safe: true });
 
 db.open(function(err, db) {
+    db.authenticate('polimuevet', 'hmipolimuevet1', function(err, success) {
+        // Do Something ...
+    });
     if(!err) {
         console.log("Connected to 'polimuevet' database");
     }
@@ -51,26 +83,8 @@ db.open(function(err, db) {
         console.log("Unable to connecto to 'polimuevet' database");
     }
 });
-*/
 
-// Pongo las 2 conexiones juntas porque a veces quiero probar en local... (comento una u otra)
-// var mongoServer = new MongoServer('127.0.0.1', 27017, {auto_reconnect: true});
-var mongoServer = new MongoServer("ds053428.mongolab.com", 53428, {auto_reconnect: true});
-db = new Db('polimuevet', mongoServer, { safe: true });
-
-db.open(function(err, db) {
- db.authenticate('polimuevet', 'hmipolimuevet1', function(err, success) {
-        // Do Something ...
-      });
- if(!err) {
-  console.log("Connected to 'polimuevet' database");
-}
-else {
-  console.log("Unable to connecto to 'polimuevet' database");
-}
-});
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // API
 var UserDAO = require('./api/dao/UserDAO');
@@ -97,25 +111,18 @@ var homeController = new HomeController();
  * DELETE: Borrar
  */
 
- app.use(express.bodyParser());
- 
- // NO CACHE
- app.use(function(req, res, next) {
-  res.header('Cache-Control','private'); 
-  next();
-});
 
- //TRAYECTOS
- app.get('/api/gettrips',tripController.getTrips)
- app.get('/api/gettrip/:id',tripController.getTrip)
- app.get('/api/getpersontrips/:id',tripController.getTripsPerson)
- app.get('/api/getinscritotrips/:id',tripController.getTripsInscrito)
- app.post('/api/newtrip',tripController.addTrip)
- app.put('/api/applytrip', tripController.applyTrip)
- app.put('/api/cancelPassenger', tripController.cancelPassenger)
- app.put('/api/updatetrip',tripController.updateTrip)
- app.delete('/api/deletetrip/:id',tripController.deleteTrip)
- ///////////////////////////////////////////////////
+//TRAYECTOS
+app.get('/api/gettrips',tripController.getTrips)
+app.get('/api/gettrip/:id',tripController.getTrip)
+app.get('/api/getpersontrips/:id',tripController.getTripsPerson)
+app.get('/api/getinscritotrips/:id',tripController.getTripsInscrito)
+app.post('/api/newtrip',tripController.addTrip)
+app.put('/api/applytrip', tripController.applyTrip)
+app.put('/api/cancelPassenger', tripController.cancelPassenger)
+app.put('/api/updatetrip',tripController.updateTrip)
+app.delete('/api/deletetrip/:id',tripController.deleteTrip)
+
 
 //USUARIOS
 app.get('/api/getusers', userController.getAllUsers);
@@ -126,13 +133,10 @@ app.delete('/api/deleteuser/:id', userController.deleteUser);
 app.put('/api/updateuser', userController.updateUser);
 app.get('/api/isuserintrip/:iduser/:idtrip', userController.isUserInTrip);
 
- //app.get('/api/getpersontrips/:id',tripController.getTripsPerson)
- //app.get('/api/getinscritotrips/:id',tripController.getTripsInscrito)
-  
- ///////////////////////////////////////////////////
 
 // API para listar Parkings
 app.get('/api/parking', parkingController.listParkings);
+
 
 // Web
 app.get('/estado-parking', homeController.estado_parking);
@@ -143,22 +147,24 @@ app.get('/registrar', homeController.registrar);
 app.get('/trayecto/:id', homeController.ver_trayecto);
 app.get('/editar-trayecto/:id', homeController.editar_trayecto);
 
+
 // Inicio de la App
 app.get('/', function (req, res) {
- res.render('home/index',
- { 
-   title : 'Home' 
- }
- )
+    res.render('home/index',
+    { 
+        title : 'Home' 
+    })
 });
 
-/////// Socket
-/// para saber los clientes que hay conectados: console.log(io.sockets.manager.connected);
+
+// Socket
+// para saber los clientes que hay conectados: console.log(io.sockets.manager.connected);
 io.of("/estado-parking").on("connection", function (socket) {
     console.log('un cliente se ha conectado a la visualizacion del parking');
     parkingManager.ee.on('parkingEvent', function(datos){
-      socket.emit('palCliente', datos);
+        socket.emit('palCliente', datos);
     });
 });
+
 
 server.listen(3000)
